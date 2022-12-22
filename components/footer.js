a312c7ecc211adaf016539d7ec82d4c8fa6dfff6
Nadav Fuchs <nadavf95@gmail.com>
2022-12-22 17:32:56 +0200

    .

--- components/footer.js
+++ components/footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
         <div className="row">
           <div className="item">
             <div className="content">
-              &copy; 2022 A Football Company
+              &copy; Lobby 99
             </div>
           </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,14 @@ import HomePage from "./homePage";
 // And showing some loading screen(s) while waiting for the data to be ready
 export default function IndexPage() {
 
-  const { data, isLoading, isError } = useApiData();
+  // const { data, isLoading, isError } = useApiData();
 
-  if (isLoading) return <Message content="Loading..." />
-  if (isError) return <Message content="An error occured..." />
-  if (!data) return <Message content="No data could be loaded..." />
+  // if (isLoading) return <Message content="Loading..." />
+  // if (isError) return <Message content="An error occured..." />
+  // if (!data) return <Message content="No data could be loaded..." />
 
-  // Just for convenience
-  const records = data.teams;
+  let data = null;
+   
   if (data){
     return (
       <>
@@ -23,8 +23,11 @@ export default function IndexPage() {
     )
   }
   else {
-    return{
+    return(
       // showing some loading/error messages
-    }
+      <div>
+        <h1> no data </h1>
+      </div>
+    )
   }
 }
