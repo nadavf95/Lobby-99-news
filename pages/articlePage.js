827a1fea95ee27f681c9f2ec7bac5ac9b70bc45a
Nadav Fuchs <nadavf95@gmail.com>
2022-12-22 15:57:11 +0200

    adding home and article pages

---
+++ pages/articlePage.js
@@ -0,0 +1,5 @@
+// an article page template. Here we are show specific article data
+
+export default function ArticlePage({article_data}) {
+
+}
---
+++ pages/homePage.js
@@ -0,0 +1,7 @@
+// Our home page. Here we are loading data "on the client"
+// And showing some loading screen(s) while waiting for the data to be ready
+
+
+export default function HomePage({data}) {
+ 
+}
