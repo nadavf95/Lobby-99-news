5e84b5ff7d0da167ec5b03d2759532508eb308c2
Nadav Fuchs <nadavf95@gmail.com>
2022-12-28 17:18:14 +0200

    working on homePage

--- pages/homePage.js
+++ pages/homePage.js
@@ -3,8 +3,26 @@
 
 
 export default function HomePage({data}) {
-
-    return{
-        //mapping articles headers item into list of  buttons itemes.
-    } 
+    if (data){
+        return(
+            //mapping articles headers item into list of  buttons itemes.
+            <div className="articles_list">
+                {data.map(article => {
+                    return(
+                        <div key={article.id} className="article_head_item">
+                            <div className="article_head_content">
+                                <div className="article_head_subject"> 
+                                    <h4>{article.subject}</h4> 
+                                </div>
+                                <div className="article_head_text">
+                                    <h2>{article.head_text}</h2>
+                                </div>
+                                <div className="article_publish_date"> {article.publish_date} </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        ) 
+    }
 }
--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,30 @@ export default function IndexPage() {
   // if (isLoading) return <Message content="Loading..." />
   // if (isError) return <Message content="An error occured..." />
   // if (!data) return <Message content="No data could be loaded..." />
-
-  let data = null;
+  let data = [{"id":"1",
+               "subject":"Traffic",
+               "head_text":"traffic in israel is bad!!!!!!!!!.",
+               "publish_date":"12/12/2022"
+              },
+              { "id":"2",
+                "subject":"Real estate",
+                "head_text":"apartments in israel are over prices!!!!!!!!!.",
+                "publish_date":"12/12/2022"
+              },    
+              { "id":"3",
+                "subject":"climate",
+                "head_text":"Israet is HOT!!!!!!!!!.",
+                "publish_date":"12/12/2022"
+              },
+              { "id":"4",
+                "subject":"animels",
+                "head_text":"Do not abuse animals!!!!!!!!!.",
+                "publish_date":"12/12/2022"
+              }        
+             ];           
    
   if (data){
+    console.log(data);
     return (
       <>
         <HomePage data={data}/>
