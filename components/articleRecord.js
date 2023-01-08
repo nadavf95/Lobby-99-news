0da764b7a3cc13b9a0e30a83c0333fe27f7e6066
Nadav Fuchs <nadavf95@gmail.com>
2023-01-08 16:35:02 +0200

    changes

--- components/articleRecord.js
+++ components/articleRecord.js
@@ -1,3 +1,17 @@
 export default function ArticleRecord({article_meta_data}){
-
+    if (article_meta_data){
+        return(
+            <div key={article.id} className="article_head_item">
+                <div className="article_head_content">
+                    <div className="article_head_subject"> 
+                        <h4>{article.subject}</h4> 
+                    </div>
+                    <div className="article_head_text">
+                        <h2>{article.head_text}</h2>
+                    </div>
+                    <div className="article_publish_date"> {article.publish_date} </div>
+                </div>
+            </div>
+        )
+    }
 }
