33aee64f2aa0c31fd6fee4fcb70bd7c2db38ef9b
Nadav Fuchs <nadavf95@gmail.com>
2023-01-12 16:44:12 +0200

    changes

--- pages/articlePage.js
+++ pages/articlePage.js
@@ -1,6 +1,6 @@
 // an article page template. Here we are show specific article data
 
-export default function ArticlePage({article_data}) {
+export default function ArticlePage({article}) {
 
     return{
         // shows aricle
