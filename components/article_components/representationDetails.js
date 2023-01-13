a109d7015ad517417893c5c29304d6ad69220f89
Nadav Fuchs <nadavf95@gmail.com>
2023-01-13 10:36:20 +0200

    working on article page

---
+++ components/article_components/representationDetails.js
@@ -0,0 +1,14 @@
+
+
+export default function RepresentationDetails({article}) {
+    return(
+        <div className="representation-details">
+            <h4>מול מי המבק בכנסת</h4>
+            <p></p>
+            <h4>מי מייצג אותם</h4>
+            <p></p>
+            <h4>מי מייצג אותנו</h4>
+            <p></p>
+        </div>
+    )
+}
