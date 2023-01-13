8ea8285b74b1961c8f67b82bd2dd2f589c66cb32
Nadav Fuchs <nadavf95@gmail.com>
2023-01-13 09:35:49 +0200

    starting to work on article page

--- pages/articlePage.js
+++ pages/articlePage.js
@@ -1,8 +1,43 @@
 // an article page template. Here we are show specific article data
+import ArticleHeader from "../components/article_components/ariticleHeader"
 
 export default function ArticlePage({article}) {
+    
+    if (article){
+        return(
+            // shows aricle
+            <div className="article-page-container">
+                <ArticleHeader article={article}/>
 
-    return{
-        // shows aricle
+                <div className="article-fight-for">
+
+                </div>
+
+        
+                <div className="action-stages-component"> 
+
+                </div>
+
+                <div className="representation-details">
+
+                </div>
+
+                <div className="first-paragraph">
+
+                </div>
+
+                <div className="image-graph">
+
+                </div>
+
+                <div className="second-paragraph">
+
+                </div>
+
+                div 
+
+
+            </div>
+        )
     }
 }
